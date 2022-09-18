cac2b261916b2a48b01cdad800f8c048ed08c30c
RelentlessNC <njconklin1987@gmail.com>
2022-09-18 11:31:33 -0500

    got all my category-routes working and handling data like they are supposed to.

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const ProductTag = require('./ProductTag');
  between Source_Model (Product) and Target_Model (Category),
  with the foreign key being defined in the Source_Model (Product). */
 Product.belongsTo(Category, {
-    foreignKey: 'product.id',
+    foreignKey: 'category_id',
 });
 
 // Categories have many Products
@@ -17,7 +17,8 @@ Product.belongsTo(Category, {
  between Source_Model (Category) and Target_Model (Product),
  with the foreign key being defined in the Target_Model (Product). */
 Category.hasMany(Product, {
-    foreignKey: 'category.id',
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE'
 });
 // Products belongToMany Tags (through ProductTag)
 /* The Source_Model.belongsToMany(Target_Model, { through: 'C (Junction_Model)' }) association means that a Many-To-Many relationship exists between Source_Model and Target_Model, using table C as junction table, which will have the foreign keys (Source_Model_Id and Target_Model_Id, for example). Sequelize will automatically create this model C (unless it already exists) and define the appropriate foreign keys on it. */
@@ -27,6 +28,7 @@ Product.belongsToMany(Tag, {
     // Tags belongToMany Products (through ProductTag)
     /* The Source_Model.belongsToMany(Target_Model, { through: 'C (Junction_Model)' }) association means that a Many-To-Many relationship exists between Source_Model and Target_Model, using table C as junction table, which will have the foreign keys (Source_Model_Id and Target_Model_Id, for example). Sequelize will automatically create this model C (unless it already exists) and define the appropriate foreign keys on it. */
 Tag.belongsToMany(Product, { through: 'C' })
+
 module.exports = {
     Product,
     Category,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,33 +8,67 @@ router.get('/', async(req, res) => {
     // be sure to include its associated Products
     try {
         const categoryData = await Category.findAll({
-            /* route works, but not getting the include Product information */
             include: [{ model: Product }],
-
         });
         res.status(200).json(categoryData);
     } catch (err) {
         res.status(500).json(err);
-        console.log(err);
     }
-
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
     // find one category by its `id` value
     // be sure to include its associated Products
+    try {
+        const categoryData = await Category.findByPk(req.params.id, {
+            include: [{ model: Product }]
+        });
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
     // create a new category
+    try {
+        const categoryData = await Category.create({
+            category_name: req.body.category_name
+        });
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(404).json(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
     // update a category by its `id` value
+    try {
+        const categoryData = await Category.update({
+            category_name: req.body.category_name
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(404).json(err);
+    }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
     // delete a category by its `id` value
+    try {
+        const categoryData = await Category.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(404).json(err);
+    }
 });
 
 module.exports = router;
